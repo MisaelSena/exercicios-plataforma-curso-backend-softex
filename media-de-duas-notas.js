2fe846c84db6a18413c5b01ffb14631a2ab882f7
Misael Sena <misaellsena@gmail.com>
2023-07-22 12:08:09 -0300

    Resolução do Exercício Média de Duas Notas

---
+++ media-de-duas-notas.js
@@ -0,0 +1,10 @@
+const nota1 = 6;
+const nota2 = 8;
+
+var media = (nota1 + nota2)/2;
+
+if(media>=7){
+  console.log(`Aluno Aprovado com Média: ${media}`);
+}else{
+  console.log(`Aluno Reprovado com Média: ${media}`);
+}
