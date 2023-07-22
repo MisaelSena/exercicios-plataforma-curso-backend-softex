c68948f512fff4b8d39017aa04436d6aa6249fda
Misael Sena <misaellsena@gmail.com>
2023-07-22 12:26:31 -0300

    Código refatorado para possibilitar usuário incluir as notas

--- media-de-duas-notas.js
+++ media-de-duas-notas.js
@@ -1,5 +1,15 @@
-const nota1 = 6;
-const nota2 = 8;
+/* No hipertexto anterior, temos um exemplo de um fluxograma que realiza o 
+ * cálculo da média de duas notas de um aluno. O fluxograma solicita duas notas 
+ * que serão somadas, e logo em seguida a média dessas notas é calculada. 
+ * Dessa forma o resultado da média quando é maior ou igual a 7 o aluno está APROVADO, 
+ * quando menor que 7 o aluno está REPROVADO.
+ */
+
+const prompt = require("prompt-sync")();
+
+
+var nota1 = parseFloat(prompt("Informe a Nota 1: "));
+var nota2 = parseFloat(prompt("Informe a Nota 2: "));
 
 var media = (nota1 + nota2)/2;
 
