545bf5700307ddf4de971c6d3413db76243d57b5
Misael Sena <misaellsena@gmail.com>
2023-08-04 21:35:02 -0300

    Conclusão do Exercício sobre Matriz Bidimensional

---
+++ matriz-bidimensional.js
@@ -0,0 +1,16 @@
+/*
+    Nesta atividade, você deve criar um código em JavaScript com 
+    uma matriz bidimensional para armazenar informações sobre animais 
+    e as suas características.
+
+    Cada linha deve representar um animal e cada coluna terá as diferentes 
+    informações sobre ele, como nome, espécie e idade.
+*/
+
+let animais = Array(3);
+
+animais.push(["Jubileu", "Cão Labrador", "10 anos"]);
+animais.push(["Bob", "Gato Persa", "2 anos"]);
+animais.push(["Bella", "Onça Pintada", "1 ano"]);
+
+console.log(animais);
