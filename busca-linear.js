0e6b5a07de2f467fe6e11fa5d1adf3312294ebca
Misael Sena <misaellsena@gmail.com>
2023-08-06 23:42:41 -0300

    Conclusão do Exercício de busca Linear! Código refatorado para mostrar quando o número não é encontrado!

--- busca-linear.js
+++ busca-linear.js
@@ -8,16 +8,26 @@
     Codifique a solução mais eficiente para buscar o número 20 no array.
 */
 
+//Como nossa lista é pequena e desordenada, a melhor opção é realizar uma busca linear.
+
 let numeros = [15, 8, 10, 25, 12, 30, 5, 20, 18, 7];
 
 const numeroBuscado = 20;
 
 function encontrar() {
     for (i = 0; i < numeros.length; i++) {
-        if(numeros[i] == numeroBuscado) {
-            console.log(i);
+        if(numeros[i] === numeroBuscado) {
+            return i;
         }   
     }
+    return -1;
+    
+}
+const indiceNumeroBuscado = encontrar();
+
+if(indiceNumeroBuscado !== -1){
+    console.log(`O elemento ${numeroBuscado} foi enconrado no índice [${indiceNumeroBuscado}]`);
+}else{
+    console.log(`O elemento ${numeroBuscado} não foi enconrado!`);
 }
 
-encontrar();
